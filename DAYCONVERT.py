770a7f4a6f9d9fcc5d410125a73ed8061134d327
jainkhushi22 <beingkhushi22@gmail.com>
2025-01-09 20:41:03 +0530

    Add files via upload

---
+++ DAYCONVERT.py
@@ -0,0 +1,11 @@
+# programme which will accept no.of days and convert into hours , minutes and seconds
+days=int(input("enter no. of days "))
+print("*"*50)
+hours=days*24
+minute=days*24*60
+seconds=days*24*60*60
+print("*"*50)
+print("number of hours in these days is = {}".format(hours) )
+print("number of mintutes in these days is = {}".format(minute) )
+print("number of second in these days is = {}".format(seconds))
+print("*"*50)
